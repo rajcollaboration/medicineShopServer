9092a3dafb189fd042b67e7397371e27e10cb8ba
rajcollabotation <washimrejakhan@gmail.com>
2023-12-12 23:39:21 +0530

    suplier functions for create add delete

---
+++ controllers/profitLoss.js
@@ -0,0 +1,9 @@
+
+
+export const getProfitLoss = async (req, res, next)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
---
+++ controllers/suplier.js
@@ -0,0 +1,24 @@
+export const addSupplier = async(req,res,next)=>{
+    try {
+        const {userName} = req.body;
+        
+    } catch (error) {
+        
+    }
+}
+
+export const deleteSuplier = async(req,res,next)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+export const editSupplier = async(req,res,next)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
